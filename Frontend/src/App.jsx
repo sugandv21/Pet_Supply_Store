be941df809deb57934a0e36b8d79f479a8d6f680
sugandv21 <sdvsuganya@gmail.com>
2025-09-27 14:00:28 +0530

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,17 +11,22 @@ import ConsultNow from "./pages/ConsultNow";
 import ProductDetail from "./pages/ProductDetail";
 import PetProducts from "./pages/PetProducts";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
+import Checkout from "./pages/checkout";
 import OrderComplete from "./pages/OrderComplete";
 import Contact from "./pages/Contact";
+import Blog from "./FPages/Blog";
+import FAQ from "./FPages/FAQ";
+import PrivacyPolicy from "./FPages/PrivacyPolicy";
+import RefundReturnsPolicy from "./FPages/RefundReturnsPolicy";
+import ShippingPolicy from "./FPages/ShippingPolicy";
+import TermsConditions from "./FPages/TermsConditions";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Navbar is always visible */}
       <Navbar />
-
-      {/* Main routed content */}
+      <ScrollToTop />
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -35,11 +40,16 @@ function App() {
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/order-complete/:identifier" element={<OrderComplete />} />
-           
+          <Route path="/order-complete/:identifier" element={<OrderComplete />} />  
+          <Route path="/blog" element={<Blog />} />    
+          <Route path="/faq" element={<FAQ />} />  
+           <Route path="/privacy-policy" element={<PrivacyPolicy />} />  
+            <Route path="/refund-policy" element={<RefundReturnsPolicy />} />  
+             <Route path="/shipping-policy" element={<ShippingPolicy />} />  
+              <Route path="/terms" element={<TermsConditions />} />  
+
         </Routes>
       </main>
-
       <Footer />
     </div>
   );
